59aea6b6a02136a86e843644f196e518a0e24d47
mikhail-webdev <78939828+mikhail-webdev@users.noreply.github.com>
2021-02-26 16:43:40 -0500

    Setting MVC structure

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require ('path');
 const express = require ('express');
 const bodyParser = require ('body-parser');
 
-const adminData = require ('./routes/admin');
+const adminRoutes = require ('./routes/admin');
 const shopRoutes = require ('./routes/shop');
+
+const errorController = require ('./controllers/error');
 
 const app = express ();
 
@@ -15,11 +17,9 @@ app.use (bodyParser.urlencoded ({extended: false}));
 
 app.use (express.static (path.join (__dirname, 'public')));
 
-app.use ('/admin', adminData.routes);
+app.use ('/admin', adminRoutes);
 app.use (shopRoutes);
 
-app.use ((req, res, next) => {
-  res.status (404).render ('404', {pageTitle: '404 Not Found'});
-});
+app.use (errorController.get404);
 
 app.listen (3000);
