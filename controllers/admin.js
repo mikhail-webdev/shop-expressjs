8c825d2c3a0a36d3932b79598610ad2e2e6e6c0d
mikhail-webdev <78939828+mikhail-webdev@users.noreply.github.com>
2021-04-07 11:30:56 -0400

    Added Async Requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -152,8 +152,8 @@ exports.postEditProduct = (req, res) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById (prodId)
     .then (product => {
       if (!product) {
@@ -163,12 +163,10 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne ({_id: prodId, userId: req.user._id});
     })
     .then (() => {
-      res.redirect ('/admin/products');
+      res.status (200).json ({message: 'Success'});
     })
     .catch (err => {
-      const error = new Error (err);
-      error.httpStatusCode = 500;
-      return next (error);
+      res.status (500).json ({message: 'Deleting product failed'});
     });
 };
 
--- routes/admin.js
+++ routes/admin.js
@@ -60,6 +60,6 @@ router.post (
   adminController.postEditProduct
 );
 
-router.post ('/delete-product', isAuth, adminController.postDeleteProduct);
+router.delete ('/product/:productId', isAuth, adminController.deleteProduct);
 
 module.exports = router;
